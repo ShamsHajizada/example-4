24e7107163dfe943e6741d0b4a2e9d1cd4219130
ShamsHajizada <mi6la68au@code.edu.az>
2024-05-23 17:24:22 +0400

    a

--- Client/src/Pages/HomePage/index.jsx
+++ Client/src/Pages/HomePage/index.jsx
@@ -4,7 +4,6 @@ const HomePage = () => {
   return (
     <div className="container">
       <section className="img-section">
-       
         <div className="txt-img">
           <p>SPRING / SUMMER COLLECTION 2017</p>
           <h1>Get up to 30% Off New Arrivals</h1>
@@ -14,30 +13,24 @@ const HomePage = () => {
 
       <section className="cards">
         <div className="women">
-          <img
-            src="https://preview.colorlib.com/theme/coloshop/images/banner_1.jpg"
-            alt=""
-          />
-          <h2>WOMEN'S</h2>
+          <div className="txt-card">
+            <h2>WOMEN'S</h2>
+          </div>
         </div>
         <div className="acc">
-          <img
-            src="https://preview.colorlib.com/theme/coloshop/images/banner_2.jpg"
-            alt=""
-          />
-          <h2>ACCESSORIES'S</h2>
+          <div className="txt-card">
+            <h2>ACCESSORIES'S</h2>
+          </div>
         </div>
         <div className="men">
-          <img
-            src="https://preview.colorlib.com/theme/coloshop/images/banner_3.jpg"
-            alt=""
-          />
-          <h2>MEN'S</h2>
+          <div className="txt-card">
+            
+            <h2>MEN'S</h2>
+          </div>
         </div>
       </section>
       <section className="login">
         <div className="txt-log">
-          {" "}
           <h1>Newsletter</h1>
           <p>Subscribe to our newsletter and get 20% off your first purchase</p>
         </div>
